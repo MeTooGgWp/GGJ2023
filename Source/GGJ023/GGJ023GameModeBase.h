f0a5eb5f42eb3c4a102d0d6367f67515a5b4b3ea
MeTooGgWp <maximecailleaux@hotmail.com>
2023-01-27 23:33:41 +0100

    Initial commit

---
+++ Source/GGJ023/GGJ023GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GGJ023GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJ023_API AGGJ023GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
